64e738be11cf8f6fb09b25b07e694563d7a29a4d
taeksin <yoyo2521@naver.com>
2024-12-06 18:23:00 +0900

    1차 테스트 완료
    일상적인얘기 처리

--- src/FAISS/Faiss_create_embeddings.py
+++ src/FAISS/Faiss_create_embeddings.py
@@ -1,25 +1,30 @@
 import pandas as pd
+from tqdm import tqdm
 from langchain.schema import Document
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 
 # 데이터 로드
-file_path = "data/우리은행_FAQ.xlsx"
+file_path = "data/data_source/woori_faq.xlsx"
 df = pd.read_excel(file_path, sheet_name="Sheet1", engine="openpyxl")
 
 # Document 및 메타데이터 생성
 documents = []
-for _, row in df.iterrows():
+for _, row in tqdm(df.iterrows(), total=len(df), desc="문서 생성 진행"):
+    # "답변" 컬럼 공백 제거
+    Q = row["질문"].strip()
+    A = row["응답"].strip()
+    
     # 텍스트 구성
-    content = f"질문: {row['질문']}\n답변: {row['응답']}"
+    content = f"질문: {Q}\n답변: {A}"
     
     # 메타데이터 구성
     metadata = {
         "은행": row["은행"],
         "1차분류": row["1차분류"],
         "2차분류": row["2차분류"],
-        "질문": row["질문"],
-        "답변": row["응답"]
+        # "질문": Q,
+        # "답변": A  # 공백 제거된 답변 사용
     }
     
     # Document 생성
